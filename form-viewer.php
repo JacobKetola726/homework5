db3816262fec828977ce41a390e5cebb090dd684
JacobKetola726 <h65k935@student.montana.edu>
2025-10-01 12:35:20 -0600

    Update form-viewer.php

--- form-viewer.php
+++ form-viewer.php
@@ -7,6 +7,11 @@ $data = $_REQUEST;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Data Viewer</title>
+    <style>
+    table { border-collapse: collapse; width: 100%; margin-top: 20px; background: white; }
+    th, td { border: 1px solid #333; padding: 10px; }
+    th { background: #ddd; }
+  </style>
 </head>
 <body>
     <h1>Submission Results</h1>
